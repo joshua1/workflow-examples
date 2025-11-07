0268438c27e3e454385e25731508a48bbe16ea86
Adrian Lam <me@adriandlam.com>
2025-11-07 12:40:05 -0800

    add nextjs

---
+++ nextjs/next.config.ts
@@ -0,0 +1,8 @@
+import { withWorkflow } from "workflow/next";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	// … rest of your Next.js config
+};
+
+export default withWorkflow(nextConfig);
